34bf34b9d51155b2f83b0bdfa8da298d459969b8
Bhanuja <bhanuja497@gmail.com>
2024-05-08 17:20:34 -0500

    nav bar services

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -5,19 +5,35 @@ import logo from "../images/frame6white.png"
 function Navbar() {
   return (
     <nav className="navbar navbar-fixed-top">
-    <div className="container">
-      <div className="navbar-logo">
-        <img src={logo} alt="Ocean Technologies Logo" />
+      <div className="container">
+        <div className="navbar-logo">
+          <img src={logo} alt="Ocean Technologies Logo" />
+        </div>
+        <div className="navbar-links">
+          <a href="about">About Us</a>
+          <a href="contact">Contact Us</a>
+          <a href="we_do">We Do</a>
+          <div className="dropdown">
+            <button className="dropbtn">Services
+              <i className="fa fa-caret-down"></i>
+            </button>
+            <div className="dropdown-content">
+              <a href="#">Banking & Insurance</a>
+              <a href="#">Financial Services</a>
+              <a href="#">Airlines & Automobiles</a>
+              <a href="#">Education</a>
+              <a href="#">Engineering</a>
+              <a href="#">Technology & Telecom</a>
+              <a href="#">Retail & Consumer services</a>
+              <a href="#">New Application Development</a>
+              <a href="#">Manufacturing</a>
+              <a href="#">Careers</a>
+            </div>
+          </div>
+          {/* Add more links as needed */}
+        </div>
       </div>
-      <div className="navbar-links">
-        <a href="#about">About Us</a>
-        <a href="#contact">Contact Us</a>
-        <a href="#services">Services</a>
-        <a href="#industry">Industry</a>
-        {/* Add more links as needed */}
-      </div>
-    </div>
-  </nav>
+    </nav>
   );
 }
 
