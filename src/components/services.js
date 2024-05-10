651617003a0f08a2c2cae94162a71c27ef5c5dc9
Bhanuja <bhanuja497@gmail.com>
2024-05-10 12:14:54 -0500

    styled the services page

--- src/components/services.js
+++ src/components/services.js
@@ -13,6 +13,34 @@ function ServiceDetails() {
                 <Navbar />
             </div>
             <div className='service_details'>
+              
+                <div className='page_left'>
+                    <div className="container">
+                        {id == 1 ? <><div className="row">
+                            <div className="col-md-6">
+                                <h2>Banking and Insurance</h2>
+                                <div className="service-image-container">
+                                    <img src="https://swisscognitive.ch/wp-content/uploads/2021/07/ai-in-banking-and-insurance-use-cases.jpeg" alt="Service" className="img-fluid" />
+                                </div>
+                                {/* <p>The standards, regulations, practices, and administrative requirements of the banking and insurance industries are always changing. A cost-controlled structure and new services were added to the banking and insurance industries' portfolios as a result of these shifting needs.
+                                        They now want multiskilled resources who are willing to adapt to changes in the industry due to the dynamic and ever-changing conditions in the field.
+                                        The greatest HRC acquisition solutions are provided by Ocean Technologies, which will enable you to efficiently and successfully manage your resources
+                                    </p> */}
+                            </div>
+                        </div>
+                            <div>
+                                <div className="col-md-6">
+                                    <div className="service-details">
+                                        <p>The standards, regulations, practices, and administrative requirements of the banking and insurance industries are always changing.<br></br> A cost-controlled structure and new services were added to the banking and insurance industries' portfolios as a result of these shifting needs.
+                                           <br></br> They now want multiskilled resources who are willing to adapt to changes in the industry due to the dynamic and ever-changing conditions in the field.<br></br>
+                                            The greatest HRC acquisition solutions are provided by Ocean Technologies, which will enable you to efficiently and successfully manage your resources
+                                        </p>
+                                    </div>
+                                </div>
+                            </div></> : <></>}
+
+                    </div>
+                </div>
                 <div className='page_right'>
                     <div className='dropdown_content'>
                         <li>
@@ -35,33 +63,6 @@ function ServiceDetails() {
                         <a href="/service/9">Manufacturing</a></li>
                         <li>
                         <a href="/service/10">Careers</a></li>
-                    </div>
-                </div>
-                <div className='page_left'>
-                    <div className="container">
-                        {id == 1 ? <><div className="row">
-                            <div className="col-md-6">
-                                <h2>Banking and Insurance</h2>
-                                <div className="service-image-container">
-                                    <img src="https://swisscognitive.ch/wp-content/uploads/2021/07/ai-in-banking-and-insurance-use-cases.jpeg" alt="Service" className="img-fluid" />
-                                </div>
-                                {/* <p>The standards, regulations, practices, and administrative requirements of the banking and insurance industries are always changing. A cost-controlled structure and new services were added to the banking and insurance industries' portfolios as a result of these shifting needs.
-                                        They now want multiskilled resources who are willing to adapt to changes in the industry due to the dynamic and ever-changing conditions in the field.
-                                        The greatest HRC acquisition solutions are provided by Ocean Technologies, which will enable you to efficiently and successfully manage your resources
-                                    </p> */}
-                            </div>
-                        </div>
-                            <div>
-                                <div className="col-md-6">
-                                    <div className="service-details">
-                                        <p>The standards, regulations, practices, and administrative requirements of the banking and insurance industries are always changing. A cost-controlled structure and new services were added to the banking and insurance industries' portfolios as a result of these shifting needs.
-                                            They now want multiskilled resources who are willing to adapt to changes in the industry due to the dynamic and ever-changing conditions in the field.
-                                            The greatest HRC acquisition solutions are provided by Ocean Technologies, which will enable you to efficiently and successfully manage your resources
-                                        </p>
-                                    </div>
-                                </div>
-                            </div></> : <></>}
-
                     </div>
                 </div>
             </div>
